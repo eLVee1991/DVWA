77f1286ff7a683cd6a3d0dcded2e3780ade23735
eLVee1991 <elvee@protonmail.com>
2017-05-28 21:07:51 +0200

    For loop used and rewritten code.

--- DVWA.py
+++ DVWA.py
@@ -1,92 +1,83 @@
 import subprocess
+import time
 
 # Vars
 singlehash = '''
 #############################################################################
 '''
+doublehash = '''
+#############################################################################
+#############################################################################
+'''
 
-print("Starting the installation")
-print("Installing php package..")
-subprocess.call('''sudo aptitude purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`''', shell=True)
-print(singlehash)
 
-print("Adding php5 repository to apt")
-subprocess.call('sudo add-apt-repository ppa:ondrej/php', shell=True)
-print(singlehash)
+installation_list = [
+'''sudo aptitude purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`''',
+'sudo add-apt-repository ppa:ondrej/php',
+'sudo apt-get update',
+'sudo apt-get upgrade',
+'sudo apt-get install php5.6',
+'sudo apt-get install php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml',
+'sudo service apache2 start',
+'sudo a2dismod php7.0',
+'sudo a2enmod php5.6',
+'sudo service apache2 restart',
+'sudo apt-get install unzip',
+'sudo apt-get install php5.6-gd',
+'sudo service apache2 restart',
+'sudo wget /var/www https://github.com/ethicalhack3r/DVWA/archive/master.zip',
+'sudo mv master.zip /var/www/',
+'sudo unzip /var/www/master.zip -d /var/www/html/'
+]
 
-print("Updating the system")
-subprocess.call('sudo apt-get update', shell=True)
-print(singlehash)
+def text():
+	print(doublehash)
+	print('''
+ ______     _      __        __    ______     ______ 
+|  ____|   | |     \ \      / /   |  ____|   |  ____|
+| |____    | |      \ \    / /    | |____    | |____ 
+|  ____|   | |       \ \  / /     |  ____|   |  ____|
+| |____    | |___     \ \/ /      | |____    | |____
+|______|   |_____|     \__/       |______|   |______|
 
-print("Upgrading the system")
-subprocess.call('sudo apt-get upgrade', shell=True)
-print(singlehash)
 
-print( "Installing php5.6")
-print( "This is the only working versions for DVWA")
-subprocess.call('sudo apt-get install php5.6', shell=True)
-print(singlehash)
+This script will setup your DVWA server. After the
+script is run, please read the instuctions and follow
+them precisely. After this your DVWA server will run.
+Just type in the [webserver ip]/DVWA-master
+''')
+	print(doublehash)
 
-print( "Installing mbstring, mcrypt mysql and xml for php5.6")
-subprocess.call('sudo apt-get install php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml', shell=True)
-print(singlehash)
+def installation():
+	for module in installation_list:
+		subprocess.call(module, shell=True)
+		time.sleep(5)
+		print("Done with: "+module)
+		print("Continuing to the next module..")
+		print(singlehash)
 
-print("I can start the apache server now so you can check in your browser if everything is working")
-subprocess.call('sudo service apache2 start', shell=True)
-
-print("You can check if the server is running now")
+text()
 question = input("Do you want to continue? (y/n): ")
 if question == "y":
-	print("Check if the php7.0 mod is disabled")
-	subprocess.call('sudo a2dismod php7.0', shell=True)
-	print(singlehash)
+		installation()
+		question2 = input("You need to continue manually from here. Do you want to continue? (y/n): ")
+		if question2 == "y":
+			print("Now we need to create a file with the config.inc.php settings so we can launch the DVWA-master server")
+			print("You will need to cd to following dir and then open the following file:")
+			print("")
+			print("cd /var/www/html/DVWA-master/config/")
+			print("sudo nano config.inc.php.dist")
+			print("Save it as a new file named: config.php.inc")
+			print("")
 
-	print("Checking if the php5.6 mod is enabled")
-	subprocess.call('sudo a2enmod php5.6', shell=True)
-	print(singlehash)
+		if question2 == "n":
+			exit()
 
-	print("Restarting the apache2 server.")
-	subprocess.call('sudo service apache2 restart', shell=True)
-	print(singlehash)
-
-	print( "Installing unzip")
-	subprocess.call('sudo apt-get install unzip', shell=True)
-	print(singlehash)
-
-	print( "Installing php5.6-gd")
-	subprocess.call('sudo apt-get install php5.6-gd', shell=True)
-	print(singlehash)
-
-	print( "Downloading the DVWA zipfile")
-	subprocess.call('sudo wget /var/www https://github.com/ethicalhack3r/DVWA/archive/master.zip', shell=True)
-	print(singlehash)
-
-	print( "Moving the file to the /var/www/ folder")
-	subprocess.call('sudo mv master.zip /var/www/', shell=True)
-	print(singlehash)
-
-	print( "Unzipping the file and moving it to /var/www/html.")
-	print( "The file will be named: DVWA-master")
-	subprocess.call('sudo unzip /var/www/master.zip -d /var/www/html/', shell=True)
-	print(singlehash)
-
-	question2 = input("You need to continue manually from here. Do you want to continue? (y/n): ")
-
-	if question2 == "y":
-		print("Now we need to create a file with the config.inc.php settings so we can launch the DVWA-master server")
-		print("You will need to cd to following dir and then open the following file:")
-		print("")
-		print("cd /var/www/html/DVWA-master/config/")
-		print("sudo nano config.inc.php.dist")
-		print("Save it as a new file named: config.php.inc")
-		print("")
-
-	if question2 == "n":
-		exit()
-	else:
-		print("Wrong input! Please answer with y/n..")
+		else:
+			print("Wrong input! Please answer with y/n..")
 
 if question == "n":
 	exit()
+
 else:
 	print("Wrong input! Please answer with y/n..")
